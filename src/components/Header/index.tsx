31e335f665b4f381255d3cee0ce07c09d1f9a677
Mahmood Yusefi <mufimf14@gmail.com>
2024-07-06 09:52:45 -0800

    some cleanup

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -66,7 +66,7 @@ const AppHeader = () => {
           },
         ]}
       />
-      <Typography.Title>Mahmoud Store</Typography.Title>
+      <Typography.Title>Store</Typography.Title>
       <AppCart/>
     </div>
   );

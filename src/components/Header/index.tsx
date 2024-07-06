3df26d11c983f01c17084fdbcffc15d416fe0e7a
Mahmood Yusefi <mufimf14@gmail.com>
2024-07-06 12:56:48 -0800

    some changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -67,7 +67,7 @@ const AppHeader = () => {
         ]}
       />
       <Typography.Title>Products Store</Typography.Title>
-      <AppCart/>
+      <AppCart />
     </div>
   );
 };
@@ -76,7 +76,7 @@ const AppCart = () => {
   const [cartDrawerOpen, setCartDrawerOpen] = useState(false)
   const [checkoutDrawerOpen, setCheckoutDrawerOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
-  
+
   useEffect(() => {
     getCart().then(res => {
       setCartItems(res.products)
@@ -92,94 +92,97 @@ const AppCart = () => {
 
   return (
     <div>
-          <Badge onClick={() => {
-            setCartDrawerOpen(true)
-          }} count={cartItems.length} className="shoppingCartIcon">
-            <ShoppingCartOutlined/>
-            </Badge>
-          <Drawer open={cartDrawerOpen} onClose={() => {
-            setCartDrawerOpen(false)
-          }}
-          title="Your Cart"
-          contentWrapperStyle={{width: 500}}
-          >
-            <Table 
-              pagination={false}
-              columns={[{
-              title: 'Title',
-              dataIndex: 'title'
-            },
-            {
-              title: 'Price',
-              dataIndex: 'price',
-              render: (value) => {
-                return <span>${value}</span>
-              }
-            }, 
-            {
-              title: 'Quantity',
-              dataIndex: 'quantity',
-              render: (value, record) => {
-                return <InputNumber min={0} defaultValue={value} onChange={(value) => {
-                  setCartItems(pre => pre.map(cart => {
-                    if (record.id === cart.id) {
-                      cart.total = (cart.price * value)
-                    }
-                    return cart
-                  }))
-                }}></InputNumber>
-              }
-            },
-            {
-              title: 'Total',
-              dataIndex: 'total',
-              render: (value) => {
-                return <span>${value}</span>
-              }
+      <div onClick={() => {
+        setCartDrawerOpen(true);
+      }} className="shoppingCartIcon">
+        <Badge count={cartItems.length}>
+          <ShoppingCartOutlined />
+        </Badge>
+      </div>
+
+      <Drawer open={cartDrawerOpen} onClose={() => {
+        setCartDrawerOpen(false)
+      }}
+        title="Your Cart"
+        contentWrapperStyle={{ width: 500 }}
+      >
+        <Table
+          pagination={false}
+          columns={[{
+            title: 'Title',
+            dataIndex: 'title'
+          },
+          {
+            title: 'Price',
+            dataIndex: 'price',
+            render: (value) => {
+              return <span>${value}</span>
             }
-            ]}
-              dataSource={cartItems}
-              summary={(data) => {
-                const total = data.reduce((pre, current) => {
-                  return pre + current.total
-              }, 0)
-              return <span>Total: ${total}</span>
-            }}
-            />
-            <Button onClick={() => {
-              setCheckoutDrawerOpen(true)
-            }} type="primary" >Checkout Your Cart</Button>
-          </Drawer>
-          <Drawer open={checkoutDrawerOpen} onClose={() => {
-            setCheckoutDrawerOpen(false)
-          }} title='Confirm Order'>
-            <Form onFinish={onConfirmOrder}>
-              <Form.Item rules={[{
-                required: true,
-                message: 'Please enter your full name'
-              }]} label='Full Name' name='full_name'>
-                <Input placeholder="Enter your full name..."/>
-              </Form.Item>
-              <Form.Item rules={[{
-                required: true,
-                type: "email",
-                message: 'Please enter your email'
-              }]} label='Email' name='email'>
-                <Input placeholder="Enter your email..."/>
-              </Form.Item>
-              <Form.Item rules={[{
-                required: true,
-                message: 'Please enter your address'
-              }]} label='Address' name='address'>
-                <Input placeholder="Enter your address..."/>
-              </Form.Item>
-              <Form.Item>
-                <Checkbox defaultChecked disabled>Cash On Delivery</Checkbox>
-              </Form.Item>
-              <Typography.Paragraph type="secondary">More methods coming soon</Typography.Paragraph>
-              <Button type="primary" htmlType="submit">Confirm Order</Button>
-            </Form>
-          </Drawer>
+          },
+          {
+            title: 'Quantity',
+            dataIndex: 'quantity',
+            render: (value: number, record: any) => {
+              return <InputNumber min={0} defaultValue={value} onChange={(value: any) => {
+                setCartItems((pre: any) => pre.map((cart: any) => {
+                  if (record.id === cart.id) {
+                    cart.total = (cart.price * value)
+                  }
+                  return cart
+                }))
+              }}></InputNumber>
+            }
+          },
+          {
+            title: 'Total',
+            dataIndex: 'total',
+            render: (value) => {
+              return <span>${value}</span>
+            }
+          }
+          ]}
+          dataSource={cartItems}
+          summary={(data) => {
+            const total = data.reduce((pre, current) => {
+              return pre + current.total
+            }, 0)
+            return <span>Total: ${total}</span>
+          }}
+        />
+        <Button onClick={() => {
+          setCheckoutDrawerOpen(true)
+        }} type="primary" >Checkout Your Cart</Button>
+      </Drawer>
+      <Drawer open={checkoutDrawerOpen} onClose={() => {
+        setCheckoutDrawerOpen(false)
+      }} title='Confirm Order'>
+        <Form onFinish={onConfirmOrder}>
+          <Form.Item rules={[{
+            required: true,
+            message: 'Please enter your full name'
+          }]} label='Full Name' name='full_name'>
+            <Input placeholder="Enter your full name..." />
+          </Form.Item>
+          <Form.Item rules={[{
+            required: true,
+            type: "email",
+            message: 'Please enter your email'
+          }]} label='Email' name='email'>
+            <Input placeholder="Enter your email..." />
+          </Form.Item>
+          <Form.Item rules={[{
+            required: true,
+            message: 'Please enter your address'
+          }]} label='Address' name='address'>
+            <Input placeholder="Enter your address..." />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox defaultChecked disabled>Cash On Delivery</Checkbox>
+          </Form.Item>
+          <Typography.Paragraph type="secondary">More methods coming soon</Typography.Paragraph>
+          <Button type="primary" htmlType="submit">Confirm Order</Button>
+        </Form>
+      </Drawer>
     </div>
   )
 }
